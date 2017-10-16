0d4c0bb8e72961b4334cb65a7c7628ea010b6196
dk <dk_gee@163.com>
2017-10-16 17:58:06 +0800

    微服务电影消费者

---
+++ microservice-consumer-movie/src/main/java/com/dkgee/cloud/user/controller/MovieController.java
@@ -0,0 +1,40 @@
+package com.dkgee.cloud.user.controller;
+
+import com.dkgee.cloud.user.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * 描述：
+ * 作者: JinHuaTao
+ * 时间：2017/10/16 9:26
+ */
+@RestController
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+    }
+
+    /**
+     * 查询microservice-provider-user服务的信息并返回
+     * */
+    @GetMapping("/user-instance")
+    public List<ServiceInstance> showInfo(){
+        return this.discoveryClient.getInstances("microservice-provider-user");
+    }
+}
